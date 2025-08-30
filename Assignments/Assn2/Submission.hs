12ccb93b9be8f34b2877545fce0d4af4528da201
Ryan-Hota <meryanhere2@gmail.com>
2025-08-30 22:45:48 +0530

    assignment tests

---
+++ Assignments/Assn2/Submission.hs
@@ -0,0 +1,11 @@
+module Assignments.Assn2.Submission ( -- Don't touch this
+    altMerge , -- Don't touch this
+    countMaxima -- Don't touch this
+) -- Don't touch this
+where -- Don't touch this
+
+altMerge :: Integer -> Integer -> Integer -- Don't touch this
+altMerge = undefined -- remove the "undefined" and write your solution here
+
+countMaxima :: (Integer -> Integer) -> (Integer,Integer) -> Integer -- Don't touch this
+countMaxima = undefined -- remove the "undefined" and write your solution here
---
+++ Assignments/Assn2/test.hs
@@ -0,0 +1,24 @@
+import qualified Assignments.Assn2.Submission as Submission
+
+altMerge :: Integer -> Integer -> Integer
+altMerge = Submission.altMerge 
+
+countMaxima :: (Integer -> Integer) -> (Integer,Integer) -> Integer 
+countMaxima = Submission.countMaxima
+
+testing :: String
+testing = 
+    if
+        and [
+            altMerge 123 456 == 142536 ,
+            altMerge 472485 573654 == 457723468554 ,
+            altMerge 1 2 == 1 ,
+        
+            countMaxima id (1, 100) == 1 ,
+            countMaxima (\x -> 3) (1, 100) == 100 ,
+            countMaxima (`mod` 2) (1, 102) == 51 
+        ]
+    then 
+        "passed all simple tests"
+    else 
+        "some test failed"
